6aa1c6acdd46cacfdfc6f3b5b0a26b1a5f4ab4af
Gotfrid <gotfridwallenstein@gmail.com>
2022-04-04 01:03:47 +0300

    Added new classes X2 & X2Test.

---
+++ src/main/java/ru/job4j/condition/X2.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class X2 {
+    public static int calc(int a, int b, int c, int x) {
+        return a * x * x + b * x + c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(X2.calc(1, 1, 1, 1));
+        System.out.println(X2.calc(0, 1, 1, 1));
+        System.out.println(X2.calc(1, 1, 0, 1));
+        System.out.println(X2.calc(1, 1, 1, 0));
+    }
+}
