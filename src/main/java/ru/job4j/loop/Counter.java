a56bb474184f75cef257bbc5bd2574b80f2ba102
Gotfrid <gotfridwallenstein@gmail.com>
2022-04-09 00:43:20 +0300

    Added method sumByEven and completed tests.

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -9,6 +9,16 @@ public class Counter {
         return sum;
     }
 
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int n = start; n <= finish; n++) {
+            if (n % 2 == 0) {
+                sum = sum + n;
+            }
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
