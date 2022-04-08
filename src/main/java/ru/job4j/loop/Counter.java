47b8904588288c915511f67d5c65594c19904276
Gotfrid <gotfridwallenstein@gmail.com>
2022-04-09 00:04:08 +0300

    Added new class Counter.

---
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,20 @@
+package ru.job4j.loop;
+
+public class Counter {
+    public static int sum(int start, int finish) {
+        int sum = 0;
+        for (int n = start; n <= finish; n++) {
+            sum = sum + n;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(0, 10));
+        System.out.println(sum(3, 8));
+        System.out.println(sum(1, 1));
+        System.out.println(sum(9, 89));
+        System.out.println(sum(3, 13));
+        System.out.println(sum(0, 15));
+    }
+}
