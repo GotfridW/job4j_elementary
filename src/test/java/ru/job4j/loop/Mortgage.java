81d714b0301d78de03737274a087d00f43fb6d0b
Gotfrid <gotfridwallenstein@gmail.com>
2022-04-10 23:34:43 +0300

    Corrected class Mortgage.

--- src/test/java/ru/job4j/loop/Mortgage.java
+++ src/test/java/ru/job4j/loop/Mortgage.java
@@ -3,9 +3,8 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        double debt = amount + amount / 100 * percent;
-        while (debt > 0) {
-            debt -= salary;
+        while (amount > 0) {
+            amount += amount / 100 * percent - salary;
             year++;
         }
         return year;
