7a3eb359574192889f821e413493a37bab074e84
Gotfrid <gotfridwallenstein@gmail.com>
2022-04-11 23:21:27 +0300

    Added new class Square.

---
+++ src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class Square {
+    public static int[] calculate(int bound) {
+        int[] rst = new int[bound];
+        for (int numbers = 0; numbers < bound; numbers++) {
+            rst[numbers]++;
+            rst[numbers] = (int) Math.pow(numbers, 2);
+        }
+        return rst;
+    }
+
+    public static void main(String[] args) {
+        int[] array = calculate(4);
+        for (int index = 0; index < array.length; index++) {
+            System.out.println(array[index]);
+        }
+    }
+}
