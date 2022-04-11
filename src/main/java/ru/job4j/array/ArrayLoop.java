310b991191c98b54aa1e2d3ad096ead708a31b58
Gotfrid <gotfridwallenstein@gmail.com>
2022-04-11 22:19:12 +0300

    Added new class ArrayLoop.

---
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class ArrayLoop {
+    public static void main(String[] args) {
+        int[] figures = new int[5];
+        for (int index = 0; index < figures.length; index++) {
+            figures[index] = index * 2 + 3;
+        }
+        for (int index = 0; index < figures.length; index++) {
+            System.out.println(figures[index]);
+        }
+    }
+}
