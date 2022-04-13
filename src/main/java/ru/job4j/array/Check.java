0d5ed3b821e7e4b5ec8cb92d85133dca342e6188
Gotfrid <gotfridwallenstein@gmail.com>
2022-04-13 13:31:43 +0300

    Method mono corrected.

--- src/main/java/ru/job4j/array/Check.java
+++ src/main/java/ru/job4j/array/Check.java
@@ -3,12 +3,12 @@ package ru.job4j.array;
 public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
-        for (boolean index : data) {
-            if (data[0] != index) {
+        for (int i = 1; i < data.length; i++) {
+            if (data[0] != data[i]) {
                 result = false;
                 break;
             }
         }
-        return  result;
+        return result;
     }
 }
