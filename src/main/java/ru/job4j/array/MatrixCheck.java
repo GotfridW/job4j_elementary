8d039d7702fb2e5f04e70e6848a73c2cdf33f9f1
Gotfrid <gotfridwallenstein@gmail.com>
2022-04-14 23:17:06 +0300

    Variable x deleted.

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -4,8 +4,7 @@ public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
         for (int i = 0; i < board.length; i++) {
-            char x = 'X';
-            if (board[row][i] != x) {
+            if (board[row][i] != 'X') {
                     result = false;
                     break;
                 }
