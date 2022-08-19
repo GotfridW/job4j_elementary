c9a3c9f2020ca578745173904144d3632971981d
Gotfrid <gotfridwallenstein@gmail.com>
2022-08-20 01:31:13 +0300

    Task [#504884] - String is an array; junit.jupiter dependency version updated.

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,32 @@
+package ru.job4j.array;
+
+import static java.lang.Character.*;
+
+public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        boolean result = false;
+        if (!name.isEmpty() && isLowerCase(name.charAt(0))) {
+            for (int i = 1; i < name.length(); i++) {
+                int code = name.codePointAt(i);
+                if (!(isDigit(code) || isSpecialSymbol(code)
+                        || isUpperLatinLetter(code) || isLowerLatinLetter(code))) {
+                    break;
+                }
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+        return code == 36 || code == 95;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        return code >= 65 && code <= 90;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        return code >= 97 && code <= 122;
+    }
+}
